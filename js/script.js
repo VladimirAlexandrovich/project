5d7e799a8c9aff2b92679d6127508c6e807c05c9
Vladimir <666traktor2012@gmail.com>
2021-03-15 13:46:12 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict"; 
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+//const obj = {
+    a: 50
+};
+
+//obj.a = 1999;
+//console.log(obj);
+
+{
+    let result = 50;
+    console.log(result);
+}
+
+const obj = {
+    name: "John",
+    age: 666,
+    isMaried: false
+};
+
+console.log(obj.age);
